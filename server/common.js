3a273695d33defabddfcd4e6a5206acf3f38f9f3
Light <death_light@foxmail.com>
2017-03-27 22:56:31 +0800

    extract common js

---
+++ server/common.js
@@ -0,0 +1,12 @@
+var filter = function () {
+    var keyWord = document.getElementById('keyWord').value;
+    var trs = document.getElementsByTagName('tr');
+    for (var i = 1; i < trs.length; i++) {
+        var tr = trs[i];
+        if (tr.innerText.indexOf(keyWord) == -1) {
+            tr.style.display = 'none';
+        } else {
+            tr.style.display = '';
+        }
+    }
+}
